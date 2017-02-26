78432f766653b784bfd08bdb303045f8f6a84f88
sThig <scott.thigpen@gmail.com>
2017-02-25 22:27:23 -0600

    I got it! Kinda... sorta...

--- cover.js
+++ cover.js
@@ -1,9 +1,11 @@
-const jobs = ['Web Designer', 'Animator', 'Developer'];
-const show = jobs[Math.floor(Math.random() * jobs.length)];
-const sentence = `<h1>Hi my name is Scott and I\'m a ${show}</h1>`;
 
+
+
+
+setInterval(
 function occupation () {
-  const intro = document.getElementById("intro").innerHTML = sentence;
-}
-
-window.onload = occupation ;
+  const jobs = ['Web Designer', 'Animator', 'Developer'];
+  const show = jobs[Math.floor(Math.random() * jobs.length)];
+  const sentence = `<h1>Hi my name is Scott and I\'m a ${show}</h1>`;
+  document.getElementById("intro").innerHTML = sentence;
+}, 1000)
